76b4ee23e088d6da68e223382b122be8fdc1ccb0
lycTJ <f29005700@gmail.com>
2022-02-17 15:47:07 +0800

    update submit form

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
-let ingredientDiv = document.querySelector('.ingredientDiv')[0];
+let ingredientDiv = document.querySelectorAll('.ingredientDiv')[0];
 
 
 
 // clone input
 addIngredientsBtn.addEventListener('click', function(){
     let newIngredients = ingredientDiv.cloneNode(true);
-    let input = newIngredients.getElementByTagName('input')[0];
+    let input = newIngredients.getElementsByTagName('input')[0];
     input.value = '';
     ingredientList.appendChild(newIngredients);
 });
